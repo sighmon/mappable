2995f46d5f5efa0166daed05064ba34c9e9655ae
pix <thatpixguy@gmail.com>
2013-06-01 18:11:20 +0930

    helper to load in some test data

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,14 @@
 class Image < ActiveRecord::Base
   attr_accessible :caption, :copyright, :fullsize_url, :latitude, :location_description, :longitude, :relevant_from, :relevant_to, :thumbnail_url
+
+  def self.find_in_range(min_lat, min_long, max_lat, max_long)
+    Image.find(:all, conditions: ['latitude BETWEEN ? AND ? AND longitude BETWEEN ? AND ?', min_lat, max_lat, min_long, max_long])   
+  end
+
+  def self.import_from_json(filename)
+    ActiveSupport::JSON.decode(File.open(filename))["features"].each do |feature|
+      properties = feature["properties"]
+      Image.create(latitude: properties["LATITUDE"], longitude: properties["LONGITUDE"], caption: properties["NAME"])
+    end
+  end
 end
