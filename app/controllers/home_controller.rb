2d5e22410f3dc6e7e996dc2924d6e321c7c3bc83
sighmon <simon.loffler@gmail.com>
2013-06-01 10:43:06 +0930

    Mekken teh rails.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  	# Set meta tags
+    set_meta_tags :title => "Exploring maps",
+                  :description => "A 2013 GovHack Adelaide entry by Hackerspace Adelaide",
+                  :keywords => "mappable, govhack, unleashed, adelaide, hackerspace, australia",
+                  :canonical => root_url,
+                  :open_graph => {
+                    :title => "Exploring maps",
+                    :description => "A 2013 GovHack Adelaide entry by Hackerspace Adelaide",
+                    :url   => root_url,
+                    # :image => URI.join(root_url, view_context.image_path('mappable@2x.png')),
+                    :site_name => "Mappable"
+                  }
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mappable::Application.config.secret_token = 'e70906c0a6c0d16b8b927d48442170467841d3dd77064ce559369b10789b419ab72f936f98b5f532a2636a23e9291d3d6db63ce78cb789549702804c631632d1'
